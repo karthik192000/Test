4cfef6c29197320f4d11567f8b07cece706d14bc
Karthik Bankapur <bankapurkarthik@gmail.com>
2025-02-02 14:49:27 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi!! This is a Java application running on a java docker agent");
+        System.out.println("Hello!! This is a Java application running on a java docker agent");
     }
 }
